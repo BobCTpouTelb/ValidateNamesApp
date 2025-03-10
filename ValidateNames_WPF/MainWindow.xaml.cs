2e5572ba03a98a5164379ca1355ea4cd42cd7ec0
Bob_CTpOuTelb <mikle0373@gmail.com>
2025-03-10 10:06:46 +0300

    Доделал проверку

--- ValidateNames_WPF/MainWindow.xaml.cs
+++ ValidateNames_WPF/MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace ValidateNames_WPF
     {
         private const string JSON_PATH = "https://localhost:7172/api/User";
         private HttpClient _httpClient;
+        string _fullname;
         public MainWindow()
         {
             InitializeComponent();
@@ -34,15 +35,12 @@ namespace ValidateNames_WPF
 
         private async void getDataBTN_Click(object sender, RoutedEventArgs e)
         {
-            var response = await _httpClient.GetStringAsync(JSON_PATH);
-            namesLV.ItemsSource = JsonConvert.DeserializeObject<List<Models.User>>(response);
             User selectedUser = namesLV.SelectedItem as User;
 
             // Если выбранный пользователь не отсутствует в переменной..
             if (selectedUser != null)
             {
                 // .. то, присваиваем ФИО в переменную _fullName, а значение из переменной _fullName передаём в сам TextBlock.
-                string _fullname;
                 fullNameTB.Text = _fullname = selectedUser.Fullname;
             }
             else // Иначе, ..
@@ -54,12 +52,37 @@ namespace ValidateNames_WPF
 
         private void resultBTN_Click(object sender, RoutedEventArgs e)
         {
+            // Создаём переменную, которая будет хранить состояние валидности строки
+            bool isValid = true;
 
+            // Проходим циклом по ФИО
+            foreach (char character in _fullname)
+            {
+                // Проверяем, является ли символ допустимым (буква или пробел)
+                if (!char.IsLetter(character) && !char.IsWhiteSpace(character))
+                {
+                    isValid = false;
+                    break; // Если нашли неподходящий символ, завершаем работу цикла
+                }
+            }
+
+            // Обновляем TextBlock
+            if (isValid)
+            {
+                resultTB.Text = "ФИО не содержит запрещённые символы.";
+                resultTB.Foreground = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                resultTB.Text = "ФИО содержит запрещённые символы.";
+                resultTB.Foreground = new SolidColorBrush(Colors.Red);
+            }
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+                var response = await _httpClient.GetStringAsync(JSON_PATH);
+                namesLV.ItemsSource = JsonConvert.DeserializeObject<List<Models.User>>(response);
         }
     }
 }
